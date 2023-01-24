65a33b86d9082f041cecac9698e1cef73e44048a
MariaKuttel <mariakuttel99@gmail.com>
2023-01-24 14:39:29 -0300

    Agregar archivos de proyecto.

---
+++ WebApiPubs/Controllers/PublisherController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Security.Policy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiPubs.Models;
+
+namespace WebApiPubs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherController : ControllerBase
+    {
+        private readonly pubsContext context;
+
+
+        public PublisherController(pubsContext context)
+        {
+            this.context = context;
+        }
+        //GET api/publisher
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Publishers>> Get()
+        {
+            return context.Publishers.ToList();
+        }
+
+        //GET BY ID
+        //GET api/publisher/:id
+        [HttpGet("{id}")]
+        public ActionResult<Publishers> GetById(string id)
+        {
+            Publishers publisher = (from a in context.Publishers
+                                   where a.PubId == id
+                                   select a).SingleOrDefault();
+            return publisher;
+        }
+
+        //POST api/publisher
+        [HttpPost]
+
+        public ActionResult Post(Publishers publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(publisher);
+            }
+            context.Publishers.Add(publisher);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //PUT api/publisher/:id
+
+        [HttpPut("{id}")]
+
+        public ActionResult Put(string id, [FromBody] Publishers publisher)
+        {
+            if (id != publisher.PubId)
+            {
+                return BadRequest();
+            }
+            context.Entry(publisher).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+        //DELETE  api/publisher/:id
+
+        [HttpDelete("{id}")]
+
+        public ActionResult<Publishers> Delete(string id)
+        {
+            var publisher = (from a in context.Publishers
+                             where a.PubId == id
+                             select a).SingleOrDefault();
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            context.Publishers.Remove(publisher);
+            context.SaveChanges();
+            return publisher;
+
+
+        }
+
+    }
+}
---
+++ WebApiPubs/Controllers/StoreController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using WebApiPubs.Models;
+
+namespace WebApiPubs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private readonly pubsContext context;
+
+
+        public StoreController(pubsContext context)
+        {
+            this.context = context;
+        }
+
+        //GET
+        [HttpGet]
+        public ActionResult<IEnumerable<Stores>> GetAllStore()
+        {
+            return context.Stores.ToList();
+        }
+
+        //GET BY ID
+        [HttpGet("{id}")]
+        public ActionResult<Stores> GetByIdStore(string id)
+        {
+            Stores store = (from s in context.Stores
+                            where s.StorId== id
+                            select s).SingleOrDefault();
+            if(store == null)
+            {
+                return NotFound();
+            }
+            return store;
+        }
+
+        //GET BY NAME
+        [HttpGet("{name}")]
+        public ActionResult<Stores> GetByNameStore(string name)
+        {
+            Stores store = (from s in context.Stores
+                            where s.StorName == name
+                            select s).SingleOrDefault();
+            if (store == null)
+            {
+                return NotFound();
+            }
+            return store;
+        }
+
+        //GET BY ZIP
+        [HttpGet("{zip}")]
+        public ActionResult<IEnumerable<Stores>> GetByZip()
+        {
+            return context.Stores.ToList();
+        }
+
+        //GET BY CITYSTATE
+        [HttpGet("{citystate}")]
+        public ActionResult<IEnumerable<Stores>> GetByCityState()
+        {
+            return null;
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult PostStore(Stores store)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(store);
+            }
+            context.Stores.Add(store);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //PUT
+        [HttpPut]
+        public ActionResult PutStore(string id, [FromBody] Stores store)
+        {
+            if (id != store.StorId)
+            {
+                return BadRequest();
+            }
+            context.Entry(store).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+        //DELETE
+        [HttpDelete]
+        public ActionResult<Stores> Delete(string id)
+        {
+            var store = (from a in context.Stores
+                             where a.StorId == id
+                             select a).SingleOrDefault();
+            if (store == null)
+            {
+                return NotFound();
+            }
+            context.Stores.Remove(store);
+            context.SaveChanges();
+            return store;
+
+
+        }
+    }
+}
